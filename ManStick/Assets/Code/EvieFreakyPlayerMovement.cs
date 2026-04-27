68f60c64236a64a15ed07d7c3b9c674ab69d17f7
Anton Ivarsson <anton.ivarsson@elev.ga.lbs.se>
2026-04-27 08:49:45 +0200

    Movement and gravity not broken and now we got legs 👽

---
+++ ManStick/Assets/Code/EvieFreakyPlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EvieFreakyPlayerMovement : MonoBehaviour
+{
+    [SerializeField] ContactFilter2D groundFilter;
+    [SerializeField] private float jumpHeight = 7f;
+    [SerializeField] private float moveSpeed = 10f;
+
+    public int maxJumps = 0;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private int extraJumps;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        Debug.Log("awake");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Debug.Log(moveInput);
+        rb.linearVelocity = moveInput * moveSpeed;
+        if (isGrounded)
+        {
+            extraJumps = maxJumps;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = rb.IsTouching(groundFilter);
+    }
+
+    void OnMove(InputValue value)
+    {
+        Debug.Log("Move");
+        moveInput = value.Get<Vector2>();
+    }
+    private void OnJump()
+    {
+        Debug.Log("Jump");
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+        else
+        {
+            if (extraJumps > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x * 1, rb.linearVelocity.y * 0);
+                rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                extraJumps--;
+            }
+        }
+    }
+}
--- ManStick/Assets/Code/PlayerMovement.cs
+++ ManStick/Assets/Code/PlayerMovement.cs
@@ -5,58 +5,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] ContactFilter2D groundFilter;
-    [SerializeField] private float jumpHeight = 7f;
-    [SerializeField] private float moveSpeed = 10f;
+    Rigidbody2D rb;
+    Vector2 moveInput;
+    [SerializeField] int moveSpeed = 1;
 
-    public int maxJumps = 0;
-
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
-    private int extraJumps;
-    private bool isGrounded;
-
-    void Awake()
+    void Start()
     {
-        Debug.Log("awake");
         rb = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        Debug.Log(moveInput);
-        rb.linearVelocity = moveInput * moveSpeed;
-        if (isGrounded)
-        {
-            extraJumps = maxJumps;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        isGrounded = rb.IsTouching(groundFilter);
     }
 
     void OnMove(InputValue value)
     {
-        Debug.Log("Move");
         moveInput = value.Get<Vector2>();
     }
-    private void OnJump()
+
+    void Update()
     {
-        Debug.Log("Jump");
-        if (isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
-        }
-        else
-        {
-            if (extraJumps > 0)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x * 1, rb.linearVelocity.y * 0);
-                rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
-                extraJumps--;
-            }
-        }
+            rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocityY);
     }
+
 }
