bd57fc9c6b9855c8c4a78c5249183da165ed2b85
EvileQwine <quinn.elhaik@elev.ga.lbs.se>
2026-04-29 12:49:10 +0200

    jump works not dash

--- ManStick/Assets/Code/PlayerMovement.cs
+++ ManStick/Assets/Code/PlayerMovement.cs
@@ -8,6 +8,16 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     Vector2 moveInput;
     [SerializeField] int moveSpeed = 2;
+    [SerializeField] int jumpHeight = 6;
+    [SerializeField] int dashStrength = 10;
+    [SerializeField] int teleportStrength = 10;
+    [SerializeField] public enum JumpAbility
+    {
+        Base,
+        Dash,
+        Teleport,
+    }
+    public JumpAbility jumpAbility;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,5 +29,29 @@ public class PlayerMovement : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+    void OnJump()
+    {
+        switch (jumpAbility)
+        {
+            case JumpAbility.Base:
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x * 1, rb.linearVelocity.y * 0);
+                rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                break;
+            case JumpAbility.Dash:
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x * 0, rb.linearVelocity.y * 0);
+                if (moveInput.x == 0 && moveInput.y == 0)
+                {
+                    rb.AddForce(Vector2.up * dashStrength, ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.AddForce(moveInput * new Vector2(dashStrength * 10, dashStrength), ForceMode2D.Impulse);
+                }
+                break;
+            case JumpAbility.Teleport:
+                rb.position = (rb.position + moveInput);
+                break;
+        }
     }
 }
