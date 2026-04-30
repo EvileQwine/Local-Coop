8eb7598efc74fd9afc93c8842ea72ddae3c1f14b
Anton Ivarsson <anton.ivarsson@elev.ga.lbs.se>
2026-04-30 13:35:18 +0200

    Player stands up

---
+++ ManStick/Assets/Code/BalanceScipt.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Balancescript : MonoBehaviour
+{
+    float targetRoatation = 0;
+    Rigidbody2D rb;
+    float force = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MoveRotation(Mathf.LerpAngle(rb.rotation, targetRoatation, force * Time.fixedDeltaTime));
+    }
+}
--- ManStick/Assets/Code/PlayerMovement.cs
+++ ManStick/Assets/Code/PlayerMovement.cs
@@ -32,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void OnJump()
     {
+        rb.linearVelocityY = 0;
         switch (jumpAbility)
         {
             case JumpAbility.Base:
