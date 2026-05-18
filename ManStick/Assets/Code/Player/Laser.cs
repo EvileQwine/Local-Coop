f3b6e04ffd12efd75511e409df0ff70ef63fdb5c
EvileQwine <quinn.elhaik@elev.ga.lbs.se>
2026-05-18 09:12:15 +0200

    laser pew pew

--- ManStick/Assets/Code/Player/Laser.cs
+++ ManStick/Assets/Code/Player/Laser.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Laser : MonoBehaviour
@@ -5,38 +6,63 @@ public class Laser : MonoBehaviour
     private bool hasLineOfSight = false;
     public Vector3 linecastEndPos;
     LineRenderer lineRenderer;
+    [SerializeField] float laserLag = 1f;
+    [SerializeField] float flashTime = 0.1f;
+    [SerializeField] float laserEndLag = 1.5f;
+    bool active = false;
     void Awake()
     {
-
+        lineRenderer = GetComponent<LineRenderer>();
     }
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
+        
     }
     public void AssignEndPoint(Vector3 point)
     {
         linecastEndPos = point;
+        StartCoroutine(LaserStart());
     }
     void Update()
     {
-        RaycastHit2D ray = Physics2D.Linecast(transform.position, linecastEndPos);
-
-        if (ray.collider != null)
+        if (active)
         {
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, linecastEndPos);
-
-            hasLineOfSight = ray.collider.CompareTag("Body Part");
-            hasLineOfSight = ray.collider.CompareTag("Bullets");
-            if (hasLineOfSight)
+            RaycastHit2D ray = Physics2D.Linecast(transform.position, linecastEndPos);
+            if (ray.collider != null)
             {
-                Debug.DrawLine(transform.position, linecastEndPos, Color.greenYellow);
+                hasLineOfSight = ray.collider.CompareTag("Body Part");
+                hasLineOfSight = ray.collider.CompareTag("Bullets");
             }
             else
             {
-                Debug.DrawLine(transform.position, linecastEndPos, Color.white);
+                hasLineOfSight = false;
             }
         }
-        hasLineOfSight = false;
+    }
+    IEnumerator LaserStart()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, linecastEndPos);
+        lineRenderer.startColor = Color.greenYellow;
+        lineRenderer.endColor = Color.yellow;
+        yield return new WaitForSeconds(laserLag);
+        StartCoroutine(Flash());
+        lineRenderer.startColor = Color.darkRed;
+        lineRenderer.endColor = Color.red;
+        active = true;
+        yield return new WaitForSeconds(laserEndLag);
+        Destroy(gameObject);
+    }
+    IEnumerator Flash()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            lineRenderer.startColor = Color.greenYellow;
+            lineRenderer.endColor = Color.yellow;
+            yield return new WaitForSeconds((flashTime/2));
+            lineRenderer.startColor = Color.darkRed;
+            lineRenderer.endColor = Color.red;
+            yield return new WaitForSeconds((flashTime / 2));
+        }
     }
 }
