a4b500c6b2d215029b55733bc84021bba7d1dc94
Anton Ivarsson <anton.ivarsson@elev.ga.lbs.se>
2026-05-18 11:04:07 +0200

    F
    ine

---
+++ ManStick/Assets/Code/Objects/UprgradeSpawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class UprgradeSpawner : MonoBehaviour
+{
+    [SerializeField] float spawnDistance = 10f;
+    Vector2 screenBounds;
+    Vector2 spawnPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
